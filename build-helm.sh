90601ef42df4a012da41b575d9a30ef65ea7e546
kenmazaika <ken@thefirehoseproject.com>
2020-05-07 11:30:52 -0700

    Initial commit

---
+++ build-helm.sh
@@ -0,0 +1,5 @@
+mkdir dist
+mkdir dist/bin
+mkdir dist/bin/linux-amd64
+
+tar -czvf dist/bin/linux-amd64/helm.tgz bin/linux-amd64/helm
---
+++ install-helm.sh
@@ -0,0 +1,5 @@
+mkdir /tmp/metropolis-utils
+curl https://storage.googleapis.com/metropolis-utils/bin/linux-amd64/helm.tgz -o /tmp/metropolis-utils/helm.tgz
+
+
+tar -zxvf /tmp/metropolis-utils/helm.tgz -C /tmp/metropolis-utils
