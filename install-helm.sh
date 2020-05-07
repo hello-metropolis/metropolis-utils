aeed2e4e0eaba0137ea302b7b8a23993d9c07ca1
kenmazaika <ken@thefirehoseproject.com>
2020-05-07 11:48:43 -0700

    adjust script

--- install-helm.sh
+++ install-helm.sh
@@ -5,7 +5,7 @@ echo "> Downloading binary"
 curl https://storage.googleapis.com/metropolis-utils/bin/linux-amd64/helm.tgz -o /metropolis-utils/helm.tgz
 
 echo "> Untaring binary"
-tar -zxvf /tmp/metropolis-utils/helm.tgz -C /metropolis-utils
+tar -zxvf /metropolis-utils/helm.tgz -C /metropolis-utils
 
 echo "> Adding helm to metropolis-utils path"
 export PATH=$PATH:/metropolis-utils/bin/linux-amd64
