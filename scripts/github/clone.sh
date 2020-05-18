cd79f17fdb7e275ae932e1096acc30636707d139
kenmazaika <ken@thefirehoseproject.com>
2020-05-18 11:51:12 -0700

    adjust

--- scripts/github/clone.sh
+++ scripts/github/clone.sh
@@ -1,17 +1,24 @@
 mkdir /root/.ssh
+echo "DEPLOY_KEY"
+echo "$DEPLOY_KEY"
+
+echo "GITHUB URL"
+echo $GITHUB_URL
+
+
+echo -n "$DEPLOY_KEY" > /root/.ssh/id_github
 
 cat /root/.ssh/id_github
-
 chmod 600 /root/.ssh/id_github
 #
-eval `ssh-agent -s`
-echo -e "Hostname github.com\nIdentityFile /root/.ssh/id_github" > /root/.ssh/known_hosts
+# eval `ssh-agent -s`
+# echo -e "Hostname github.com\nIdentityFile /root/.ssh/id_github" > /root/.ssh/known_hosts
 #
-for domain in "github.com"; do
-  sed -i "/$domain/d" /root/.ssh/known_hosts
-  line=$(ssh-keyscan $domain,`nslookup $domain | awk '/^Address: / { print $2 ; exit }'`)
-  echo $line >> /root/.ssh/known_hosts
-done
+# for domain in "github.com"; do
+#   sed -i "/$domain/d" /root/.ssh/known_hosts
+#   line=$(ssh-keyscan $domain,`nslookup $domain | awk '/^Address: / { print $2 ; exit }'`)
+#   echo $line >> /root/.ssh/known_hosts
+# done
 #
-git clone $GITHUB_URL . --depth=1
+# git clone $GITHUB_URL . --depth=1
 echo "finish"
