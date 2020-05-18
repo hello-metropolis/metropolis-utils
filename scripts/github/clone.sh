6d5fe5a3cc8c7a40316e0afdfd740b00bf7fa8b3
kenmazaika <ken@thefirehoseproject.com>
2020-05-18 12:24:06 -0700

    adjust

--- scripts/github/clone.sh
+++ scripts/github/clone.sh
@@ -1,20 +1,18 @@
 mkdir /root/.ssh
 
+# Setup the id_github SSH key
 echo -n "$DEPLOY_KEY\n" > /root/.ssh/id_github
-
-cat /root/.ssh/id_github
 chmod 600 /root/.ssh/id_github
 
-
+# Add to known_hosts
 echo "Hostname github.com\nIdentityFile /root/.ssh/id_github" > /root/.ssh/known_hosts
-
-echo "Adding to known_hosts"
 for domain in "github.com"; do
   sed -i "/$domain/d" /root/.ssh/known_hosts
   line=$(ssh-keyscan $domain,`nslookup $domain | awk '/^Address: / { print $2 ; exit }'`)
   echo $line >> /root/.ssh/known_hosts
 done
 
+# Setup the .clone file with the commands to run to clone.
 mkdir /metropolis-utils/
 echo "eval \`ssh-agent -s\`" >> /metropolis-utils/.clone
 echo "ssh-add /root/.ssh/id_github" >> /metropolis-utils/.clone
