ba7b38e5d923b26b77083e014a8196b30487b720
kenmazaika <ken@thefirehoseproject.com>
2020-05-18 12:11:58 -0700

    adjust

--- scripts/github/clone.sh
+++ scripts/github/clone.sh
@@ -12,16 +12,13 @@ cat /root/.ssh/id_github
 chmod 600 /root/.ssh/id_github
 #
 
-echo -e "Hostname github.com\nIdentityFile /root/.ssh/id_github" > /root/.ssh/known_hosts
+echo "Hostname github.com\nIdentityFile /root/.ssh/id_github" > /root/.ssh/known_hosts
 
-echo "known hosts"
-cat /root/.ssh/known_hosts
-#
-# for domain in "github.com"; do
-#   sed -i "/$domain/d" /root/.ssh/known_hosts
-#   line=$(ssh-keyscan $domain,`nslookup $domain | awk '/^Address: / { print $2 ; exit }'`)
-#   echo $line >> /root/.ssh/known_hosts
-# done
+for domain in "github.com"; do
+  sed -i "/$domain/d" /root/.ssh/known_hosts
+  line=$(ssh-keyscan $domain,`nslookup $domain | awk '/^Address: / { print $2 ; exit }'`)
+  echo $line >> /root/.ssh/known_hosts
+done
 #
 # git clone $GITHUB_URL . --depth=1
 echo "finish"
