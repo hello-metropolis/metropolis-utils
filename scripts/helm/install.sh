240b6664ac0f660fb837666b776561319a46d796
kenmazaika <ken@thefirehoseproject.com>
2020-06-08 11:10:09 -0700

    Use curl -sS to download binaries that are installed so the output is less verbose.

--- scripts/helm/install.sh
+++ scripts/helm/install.sh
@@ -2,7 +2,7 @@ echo "Metropolis Utils - Adding Helm"
 mkdir /metropolis-utils
 
 echo "> Downloading binary"
-curl https://storage.googleapis.com/metropolis-utils/bin/linux-amd64/helm.tgz -o /metropolis-utils/helm.tgz
+curl -sS https://storage.googleapis.com/metropolis-utils/bin/linux-amd64/helm.tgz -o /metropolis-utils/helm.tgz
 
 echo "> Untaring binary"
 tar -zxvf /metropolis-utils/helm.tgz -C /metropolis-utils
--- scripts/terraform/install.sh
+++ scripts/terraform/install.sh
@@ -2,7 +2,7 @@ echo "Metropolis Utils - Adding Helm"
 mkdir /metropolis-utils
 
 echo "> Downloading binary"
-curl https://storage.googleapis.com/metropolis-utils/bin/linux-amd64/terraform.tgz -o /metropolis-utils/terraform.tgz
+curl -sS https://storage.googleapis.com/metropolis-utils/bin/linux-amd64/terraform.tgz -o /metropolis-utils/terraform.tgz
 
 echo "> Untaring binary"
 tar -zxvf /metropolis-utils/terraform.tgz -C /metropolis-utils
